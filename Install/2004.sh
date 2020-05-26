91600a0712e90997995da556f007d2c4d517938e
Gleiton Campos <stuard.br@gmail.com>
2020-05-26 14:27:02 -0300

    Update 2004.sh

--- Install/2004.sh
+++ Install/2004.sh
@@ -12,9 +12,9 @@ apt install -y linux-headers-generic build-essential dkms  # && \
 rm /tmp/finish
 
 
-wget https://repo.zabbix.com/zabbix/4.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_4.4-1+focal_all.deb -O zabbix-4.4.1.deb
+wget https://repo.zabbix.com/zabbix/4.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_4.0-1+focal_all.deb -O zabbix-4.0.deb
 #dpkg -i zabbix-release_4.0-2+bionic_all.deb
-dpkg -i zabbix-4.4.1.deb 
+dpkg -i zabbix-4.0.deb 
 apt update 
 apt install -y build-essential module-assistant 
 apt install -y locales  
